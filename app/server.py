f6951096b86578f883206a477565338e81ed0e24
Ahmadlhm <lhmahmad2@gmail.com>
2025-06-25 15:40:30 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
-import joblib
+from pydantic import BaseModel
 import numpy as np
+import joblib
 
 
-model = joblib.load('app/rf.joblib')
+model = joblib.load('app/best_credit_score_model.pkl')
 
 class_names = np.array(['Poor', 'Standard', 'Good'])
-
 app = FastAPI()
+
+class InputData(BaseModel):
+    features: list
 
 @app.get('/')
 def read_root():
     return {'message': 'Bank Credit Classification model API'}
-
 @app.post('/predict')
-def predict(data: dict):
-    features = np.array(data['features']).reshape(1, -1)
+def predict(data: InputData):
+    features = np.array(data.features).reshape(1, -1)
     prediction = model.predict(features)
-    class_name = class_names[prediction][0]
-    return {'predicted_class': class_name}
+    return {'predicted_class': class_names[int(prediction[0])]}
+
+
